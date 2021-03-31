dfde042788fa4ad94b5d63d84acbef45e0aafb79
Doug <doug.kerzner@gmail.com>
2021-03-31 11:39:26 -0400

    Event listener on ballparks

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@
 const loginDiv = document.querySelector('div#login-container')
 const divMain = document.querySelector('div#main-container')
 const wishlistUl = document.createElement('ul')
+const visitedUl = document.createElement('ul')
 const wishlist = document.querySelector('div#wishlist')
+const visitedDiv = document.querySelector('div#visited')
+const ballparks = document.querySelector('header.main-title')
+const ballparkUl = document.createElement('ul')
 
 
 
@@ -35,27 +39,47 @@ loginDiv.addEventListener('submit', event => {
         fetch(`http://localhost:3000/users/${username}`)
             .then(resp => resp.json())
             .then(data => {
-                // console.log(data)
+                console.log(data.ballparks)
                 divMain.style.display = 'block'
                 loginDiv.style.display = 'none'
                 wishlist.append(wishlistUl)
+                visitedDiv.append(visitedUl)
+                ballparks.append(ballparkUl)
+                data.ballparks.forEach(ballpark =>{
+                    // console.log(ballpark.name)
+                    const ballparkLi = document.createElement('li')
+                    ballparkLi.dataset.id = ballpark.id 
+                    ballparkLi.textContent = ballpark.home_team
+                    ballparkUl.append(ballparkLi)
+
+                })
                 data.user_ballparks.forEach(visit => {
                     if(visit.wishlist === true){
                         const ballparkId = (visit.ballpark_id)
                         // console.log(data)
                         const wishLi = document.createElement('li')
                         wishLi.innerText = data.ballparks[(ballparkId)- 1].name 
-                        console.log(data.ballparks[ballparkId - 1].name )
                         wishlistUl.append(wishLi)
+                    } else if (visit.visited === true){
+                        const ballparkId = visit.ballpark_id
+                        const visitLi = document.createElement('li')
+                        visitLi.innerText = data.ballparks[(ballparkId)- 1].name 
+                        visitedUl.append(visitLi)
                     }
-                    // console.log(visit)
+                    
                 })
-            
                 
             })
-
     }
+})
 
-
+ballparks.addEventListener('click', event => {
+    if(event.target.matches('li')){
+        fetch(`http://localhost:3000/ballparks/${event.target.dataset.id}`)
+        .then(resp => resp.json())
+        .then(data => {
+            console.log(data)
+        })
+    }
 })
 
